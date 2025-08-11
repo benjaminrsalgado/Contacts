f83614bf2a665c6db378131e6f63e7a0650f665b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-08-11 13:07:11 -0600

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Benjamin Rojo on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
